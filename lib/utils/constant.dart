fadd580471a1fbeb3aee5b324ee4362d7e2235e3
Robert <robertm3lendez@gmail.com>
2019-12-22 13:33:12 -0500

    dd

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
 
-//const kUrl = "https://intense-coast-64168.herokuapp.com";
-const kUrl = 'http://localhost:3000';
-// const Map<String, dynamic> kHeaders = {
-//   "Content-type": "application/json",
-//   "x-auth-token": '',
-
-//   // or whatever
+const kUrl = "https://intense-coast-64168.herokuapp.com";
 
 const kTextStyleDetails = TextStyle(fontSize: 18);
 const kReferralCardTextStyle = TextStyle(fontSize: 16.0);
